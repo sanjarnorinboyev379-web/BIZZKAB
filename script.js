e4ffc4a39a12ed309c0fa35c3bffc57e03db8b68
BIZZKAB <sanjarnorinboyev379@gmail.com>
2026-01-31 18:56:58 +0500

    Refactor add function and improve search functionality

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
-function add(item) {
-    alert(item + " savatchaga qo‘shildi ✅");
+let count = 0;
+let total = 0;
+
+function add(btn) {
+    let card = btn.parentElement;
+    let name = card.dataset.name;
+    let price = Number(card.dataset.price);
+
+    count++;
+    total += price;
+
+    document.getElementById("count").innerText = count;
+    document.getElementById("total").innerText = total;
+
+    let li = document.createElement("li");
+    li.innerText = name + " - " + price + " so'm";
+
+    document.getElementById("items").appendChild(li);
 }
 
 const search = document.getElementById("search");
 
 search.addEventListener("keyup", function () {
     let value = search.value.toLowerCase();
-    let cards = document.querySelectorAll(".card");
-
-    cards.forEach(card => {
-        let text = card.innerText.toLowerCase();
-        card.style.display = text.includes(value) ? "block" : "none";
+    document.querySelectorAll(".card").forEach(card => {
+        card.style.display =
+            card.innerText.toLowerCase().includes(value)
+                ? "block"
+                : "none";
     });
 });
